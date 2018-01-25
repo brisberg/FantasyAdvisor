7e245b3d7ad133767e8f7543601708deed1f1655
brisberg <brandon.risberg@gmail.com>
2018-01-25 11:25:09 -0800

    temporarily switching back to api root view, startproject jumped ahead to router

--- server/fantasyadvisor/yahoo/urls.py
+++ server/fantasyadvisor/yahoo/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from yahoo import views
 
 urlpatterns = [
-    url(r'^leagues/$', views.LeagueList.as_view()),
-    url(r'^leagues/(?P<pk>[0-9]+)/$', views.LeagueDetail.as_view()),
+    url(r'^leagues/$', views.LeagueList.as_view(), name="league-list"),
+    url(r'^leagues/(?P<pk>[0-9]+)/$', views.LeagueDetail.as_view(), name="league-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
