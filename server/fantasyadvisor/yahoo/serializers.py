a28f0c2e716c73b299ca4d835d73adc1bfd79f91
brisberg <brandon.risberg@gmail.com>
2018-01-25 10:12:00 -0800

    replaced serializer with generic ModelSerializer shortcut

--- server/fantasyadvisor/yahoo/serializers.py
+++ server/fantasyadvisor/yahoo/serializers.py
@@ -13,23 +13,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('url', 'name')
 
-
-class LeagueSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    game_code = serializers.CharField(max_length=10)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `League` instance, given the validated data.
-        """
-        return League.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `League` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.game_code = validated_data.get('game_code', instance.game_code)
-        instance.save()
-        return instance
+class LeagueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = ('id', 'title', 'game_code')
