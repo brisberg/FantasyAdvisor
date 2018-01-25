69b710fdb236d1ac0c27774b4f0c44f78babc90c
brisberg <brandon.risberg@gmail.com>
2018-01-25 10:47:46 -0800

    Tutorial pt3 done. Converted to pre-mixed generic views. so small

--- server/fantasyadvisor/yahoo/views.py
+++ server/fantasyadvisor/yahoo/views.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework import status
-from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from django.http import Http404
 
 from yahoo.models import League
 from yahoo.serializers import UserSerializer, GroupSerializer, LeagueSerializer
@@ -27,36 +21,11 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class LeagueList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    """
-    List all leagues, or create a new league.
-    """
+class LeagueList(generics.ListCreateAPIView):
     queryset = League.objects.all()
     serializer_class = LeagueSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class LeagueDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    """
-    Retrieve, update or delete a league.
-    """
+class LeagueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = League.objects.all()
     serializer_class = LeagueSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
