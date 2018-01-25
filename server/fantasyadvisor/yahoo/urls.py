37585555067fa90cd1d7f3ddc1dc53391d2c5012
brisberg <brandon.risberg@gmail.com>
2018-01-25 10:40:07 -0800

    Converted to class based views

--- server/fantasyadvisor/yahoo/urls.py
+++ server/fantasyadvisor/yahoo/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from yahoo import views
 
 urlpatterns = [
-    url(r'^leagues/$', views.league_list),
-    url(r'^leagues/(?P<pk>[0-9]+)/$', views.league_detail),
+    url(r'^leagues/$', views.LeagueList.as_view()),
+    url(r'^leagues/(?P<pk>[0-9]+)/$', views.LeagueDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- server/fantasyadvisor/yahoo/views.py
+++ server/fantasyadvisor/yahoo/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from yahoo.serializers import UserSerializer, GroupSerializer
 
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from django.http import Http404
 
 from yahoo.models import League
-from yahoo.serializers import LeagueSerializer
+from yahoo.serializers import UserSerializer, GroupSerializer, LeagueSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -29,44 +24,46 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-@api_view(['GET', 'POST'])
-def league_list(request, format=None):
+class LeagueList(APIView):
     """
     List all leagues, or create a new league.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         leagues = League.objects.all()
         serializer = LeagueSerializer(leagues, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-    serializer = LeagueSerializer(data=request.data)
+    def post(self, request, format=None):
+        serializer = LeagueSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def league_detail(request, pk, format=None):
+class LeagueDetail(APIView):
     """
     Retrieve, update or delete a league.
     """
-    try:
-        league = League.objects.get(pk=pk)
-    except League.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return League.objects.get(pk=pk)
+        except League.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        serializer = LeagueSerializer(snippet)
+    def get(self, request, pk, format=None):
+        league = self.get_object(pk)
+        serializer = LeagueSerializer(league)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        league = self.get_object(pk)
         serializer = LeagueSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        league = self.get_object(pk)
         league.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
