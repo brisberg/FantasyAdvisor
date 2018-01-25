630d8df73d5fc287f4e79ce55b43cd8ba5df954c
brisberg <brandon.risberg@gmail.com>
2018-01-25 11:44:17 -0800

    refactored to use viewSets, explicitly binding viewset actions to urls

--- server/fantasyadvisor/yahoo/urls.py
+++ server/fantasyadvisor/yahoo/urls.py
@@ -2,9 +2,37 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from yahoo import views
 
-urlpatterns = [
-    url(r'^leagues/$', views.LeagueList.as_view(), name="league-list"),
-    url(r'^leagues/(?P<pk>[0-9]+)/$', views.LeagueDetail.as_view(), name="league-detail"),
-]
+from yahoo.views import LeagueViewSet, UserViewSet, GroupViewSet, api_root
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+league_list = LeagueViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+league_detail = LeagueViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+group_list = GroupViewSet.as_view({
+    'get': 'list'
+})
+group_detail = GroupViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = format_suffix_patterns([
+    url(r'^$', api_root),
+    url(r'^leagues/$', league_list, name='league-list'),
+    url(r'^leagues/(?P<pk>[0-9]+)/$', league_detail, name='league-detail'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^groups/$', group_list, name='group-list'),
+    url(r'^groups/(?P<pk>[0-9]+)/$', group_detail, name='group-detail'),
+])
--- server/fantasyadvisor/yahoo/views.py
+++ server/fantasyadvisor/yahoo/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -14,11 +13,12 @@ from yahoo.permissions import IsOwnerOrReadOnly
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        # 'users': reverse('user-list', request=request, format=format),
+        'users': reverse('user-list', request=request, format=format),
+        'groups': reverse('group-list', request=request, format=format),
         'leagues': reverse('league-list', request=request, format=format),
     })
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
@@ -33,17 +33,16 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class LeagueList(generics.ListCreateAPIView):
-    queryset = League.objects.all()
-    serializer_class = LeagueSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+class LeagueViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class LeagueDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
     queryset = League.objects.all()
     serializer_class = LeagueSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly)
+                          IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
