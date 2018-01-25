2442039b7f1a871538c415e731a636d8a103a006
brisberg <brandon.risberg@gmail.com>
2018-01-25 11:53:16 -0800

    Tutorial pt6 done. Using a router within the app to configure the urls

--- server/fantasyadvisor/yahoo/urls.py
+++ server/fantasyadvisor/yahoo/urls.py
@@ -1,38 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
 from yahoo import views
 
-from yahoo.views import LeagueViewSet, UserViewSet, GroupViewSet, api_root
+# Create a router and register our viewsets with it.
+router = routers.DefaultRouter()
+router.register(r'leagues', views.LeagueViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
-league_list = LeagueViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-league_detail = LeagueViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-group_list = GroupViewSet.as_view({
-    'get': 'list'
-})
-group_detail = GroupViewSet.as_view({
-    'get': 'retrieve'
-})
-
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-    url(r'^leagues/$', league_list, name='league-list'),
-    url(r'^leagues/(?P<pk>[0-9]+)/$', league_detail, name='league-detail'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
-    url(r'^groups/$', group_list, name='group-list'),
-    url(r'^groups/(?P<pk>[0-9]+)/$', group_detail, name='group-detail'),
-])
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
